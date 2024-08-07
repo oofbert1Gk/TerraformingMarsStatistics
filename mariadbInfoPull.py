1ad42f6d2e96a63dfeb47622e4a1e7088531ea3a
foo <ih20058@thameshigh.school.nz>
2024-08-08 10:08:04 +1200

    Added Custom Mariab Query Ability and added tables and some minor cleaning

--- mariadbInfoPull.py
+++ mariadbInfoPull.py
@@ -4,6 +4,7 @@ import numpy as np
 import random 
 import matplotlib.pyplot as plt
 import mysql.connector
+import tabulate
 import sys
 
 
@@ -16,46 +17,70 @@ cursor = cnx.cursor()
 
 #Creating scatter plot which compares two columns, this will only work if both of the collumns only contain ints
 
-def scatter2columns():
+def getData(name):
 
-#getting input
-    #xTable=input("Enter table for x collumn: ")
-    #yTable=input("Enter table for y collumn: ")
-    #xColumn=input("Enter collumn to select for x: ")
-    #yColumn=input("Enter collumn to select for y: ")
-    xTable="ScoreByCategory"
-    yTable="ScoreByCategory"
-    xColumn="greeneries"
-    yColumn="cities"
+    #getting table name input
+    xTable=input("Enter table for " + name +" column: ")
+    name=input("Enter column to select from "+ xTable +": ")
+    
+    #reading data 
+    xQuery=cursor.execute("select " + name + "  from " + xTable)
+    value=cursor.fetchall()
 
-#reading data from mariadb 
-    xQuery=cursor.execute("select " + xColumn + "  from " + xTable)
-    x=cursor.fetchall()
-    yQuery=cursor.execute("select " + yColumn + "  from " + yTable)
-    y=cursor.fetchall()
-
-
-    for i in range(len(x)):       #looping over the columns and converting them from tuple to int 
+    for i in range(len(value)):       #looping over the columns and converting them from tuple to int 
         try:
-            x[i]=int(x[i][0])
-            y[i]=int(y[i][0])
+            value[i]=int(value[i][0])
         except ValueError:
             input("collumn must only contain floats")
-            
-        
-    plt.xlabel("points from " + xColumn)  #adding labels 
-    plt.ylabel("points from" + yColumn)
+    return([value,name])
+
+def scatter2columns():
+    x=getData("x")
+    y=getData("y")
+
+    plt.xlabel("points from " + x[1])  #adding labels 
+    plt.ylabel("points from" + y[1])
 
     #calculating the trendline equation and converting it to right form 
-    b=np.polyfit(x, y, 1)
+    b=np.polyfit(x[0], y[0], 1)
     p=np.poly1d(b)
     
     #defining graphs 
-    plt.scatter(x, y)
-    plt.plot(x, p(x), color='red')
+    plt.scatter(x[0], y[0])
+    plt.plot(x[0], p(x[0]), color='red')
     
     plt.show()
 
+def customQueryToTable():
 
+    
+    cursor.execute(input("Enter mysql query: "))
+    fieldNames=[i[0] for i in cursor.description]
+    result=tabulate.tabulate(cursor.fetchall(), headers=fieldNames, tablefmt='psql')
+    print(result)
+
+
+def infoDump2columns():
+
+    x=getData("x")
+    y=getData("y")
+ 
+    
+    #Making Table
+    table=""
+    for i in range(len(x[0])):
+        
+        a=str(x[0][i])+"|"+str(y[0][i])+"\n"
+        
+        
+        for i in range(len(a)):
+            a=a+"-"
+        
+        table=table+"\n"+a
+        
+    
+    print(table)
+    
+customQueryToTable()
 cursor.close()
 cnx.close()
