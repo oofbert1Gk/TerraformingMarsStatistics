88f5e05a00cf2f9b89fcbcb68d42d62f831a2300
foo <ih20058@thameshigh.school.nz>
2024-09-06 09:13:19 +1200

    Finished inital version of showing tables off custom mysql queries

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from IPython import display
 from enum import Enum
 from pprint import pprint
 from fasthtml.common import *
+from fasthtml.common import Table as TB 
 from fastcore.test import *
 from starlette.testclient import TestClient
 from starlette.requests import Headers
@@ -27,19 +28,71 @@ import sys #system specific parameters and functions
 import subprocess #running shell commands
 
 css = Style(':root { --pico-font-size: 100%; --pico-font-family: Pacifico;}','table tr td {\r\n  border-right: 1px solid blue;\r\n  color: black\r\n}\r\n\r\ntable tr td:last-of-type {\r\n  border: none;\r\n}')
-app = FastHTML(hdrs=(picolink, css))
+app = FastHTML(hdrs=(picolink, css), static_dir='./static')
 client=TestClient(app)
 
-subprocess.run(["bash","getter.sh",""])
+def pageSelect():
+    pages = [('Insert', "/"), ("Game Statistics", "/GStats"), ("Card Statistics", "/CStats"), ("Help&Info", "/info")]
+    return Container(Navbar('nav', 'selidx', items=pages, cls='navbar-light bg-secondary rounded-lg',
+                            image='logo.svg', hdr_href="http://0.0.0.0:5001/",
+                            placement=PlacementT.Default, expand=SizeT.Md, toggle_left=False))  
+
+def Table(cmd):
+    #opening connection
+    cnx = mysql.connector.connect(user='pythonRead', password='Ehaid4Zah5vootheeCh3euh1thie4A',host='127.0.0.1',database='tfm')
+    cursor = cnx.cursor()
+    
+    #getting information from the database
+    cursor.execute(cmd)
+    fieldNames=[i[0] for i in cursor.description]
+    result=cursor.fetchall()
+
+    #defining table 
+    table=()
+    headers=()
+    nColumn=len(fieldNames)
+    for i in range(nColumn):
+        headers+=(Th(fieldNames[i]),)
+        
+    table+=Tr(headers),
+    for i in range(len(result)):
+        data=()
+        for j in range(nColumn):
+            data+=Td(result[i][j]),
+        table+=Tr(data),
+
+    cursor.close()
+    cnx.close()
+    #table=Table(table)
+    return(TB(table))
 
 @app.get("/")
 def home():
-    return [
-        Form([
-            Label("Link:"),
-            Input(type="text", name="user_input", placeholder="Type here..."),
-            Button("Submit", type="submit")
-        ], action="/", method="post")
-    ]
+    return Main(pageSelect(),P("Insert Link:"),
+                Form(Input(type="text", name="data"),
+                     Button("Submit"),
+                     action="/", method="post"))
+
+@app.post("/")
+def add_message(data:str):
+    print("getting data")
+    subprocess.run(["bash","getter.sh",data])
+    return home()
+
+tableView=()
+      
+@app.get("/GStats")
+def page2():
+    return Main(pageSelect(),P("Enter mysql query:"),
+                Form(Input(type="text", name="data"),
+                     Button("Submit"),
+                     action="/GStats", method="post"),tableView)
+
+@app.post("/GStats")
+def add_message(data:str):
+    global tableView
+    tableView=(Table(data),)+tableView
+    return page2()  
 
 serve()
+
